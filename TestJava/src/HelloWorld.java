1a5c21fe8f0b4c509e8a7bf78ed1a9eedafdde6b
PunniamoorthyRa <PunniamoorthyRa@ONOTTAA696986P.AGR.GC.CA>
2020-05-09 13:52:09 -0400

    This is my second change

--- TestJava/src/HelloWorld.java
+++ TestJava/src/HelloWorld.java
@@ -1,9 +1,14 @@
 
 public class HelloWorld {
-
+    
+	public static String sayHello(String myInput) {
+		return "Hello world form function";
+		
+	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello World");
+		sayHello("Ravi");
 
 	}
 
